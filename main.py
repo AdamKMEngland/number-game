a743e12d791d5a09e5846ce19bce52a6813ca62e
Adam England <monofeaze@gmail.com>
2021-02-08 18:27:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Number guessing game
+
+# The computer will generate a number
+import random
+
+computer_guess = random.randInt(0,10)
+print(computer_guess)
+# The human will gues a number 
+
+
+# Check if the human guessed the correct number 
+
+
+# If he/she did, print a congratz message 
+
+
+# If he/she did not, print a boo hoo message 
+
+
